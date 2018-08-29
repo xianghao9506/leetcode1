30cd51741f9e22649758290073ea27b34f4427e9
xianghao9506 <38573146+xianghao9506@users.noreply.github.com>
2018-08-29 13:22:10 -0700

    Add files via upload

---
+++ 888. Fair Candy Swap.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fairCandySwap(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        Sa, Sb = sum(A), sum(B)
+        setB = set(B)
+        for x in A:
+            if x + (Sb - Sa) / 2 in setB:
+                return [x, x + (Sb - Sa) / 2]
